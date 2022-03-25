32652b7db9292ac1feebc30c62d9101cb2ae1a04
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-03-25 11:55:53 +0600

    Citizen_example

---
+++ src/problem5/CitizenExample.java
@@ -0,0 +1,27 @@
+package problem5;
+
+public class CitizenExample {
+
+    public static void main(String[] args) {
+
+        Citizen [] arr = new Citizen[4];
+
+        arr[0]= new Citizen(101,"Rahim",1234);
+        arr[1]= new Citizen(102,"Karim",1234,12000);
+        arr[2]= new Citizen(103,"Salam",1234,25000);
+        arr[3]= new Citizen(104,"Kuddus",1234,16000);
+
+        int count=0;
+
+        for(Citizen c:arr)
+        {
+            if(c.salary<20000)
+            {
+                count++;
+                System.out.println("Name: " +c.name);
+            }
+        }
+        System.out.println(count +" "+"Citizen Salary is less than 20,000");
+    }
+    
+}
