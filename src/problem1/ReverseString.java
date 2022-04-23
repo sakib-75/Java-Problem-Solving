9ba47f8814b237cf5e35c0ee0a6017476e3df986
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-04-23 21:36:50 +0600

    problem 1 ReverseString update

--- src/problem1/ReverseString.java
+++ src/problem1/ReverseString.java
@@ -1,28 +1,27 @@
 package problem1;
+
 import java.util.Scanner;
 
 //java program to reverse a string
 public class ReverseString {
 
-	@SuppressWarnings("resource")
-	public static void main(String[] args) {
-		
-		Scanner inp = new Scanner(System.in);
-		System.out.print("Enter a string: ");
-		String user_str = inp.nextLine(), reversed="";
-		char ch;
-		
-		System.out.println("Original string: " + user_str);
-		
-		for (int i=0; i<user_str.length(); i++)
-		{
-			ch = user_str.charAt(i);
-			reversed = ch + reversed;
-		}
-		
-		System.out.println("Reversed string: " + reversed);
-		
+    public static void main(String[] args) {
 
-	}
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String user_str = inp.nextLine();
+        StringBuilder reversed = new StringBuilder();
+        char ch;
+
+        System.out.println("Original string: " + user_str);
+
+        for (int i = 0; i < user_str.length(); i++) {
+            ch = user_str.charAt(i);
+            reversed.insert(0, ch);
+        }
+
+        System.out.println("Reversed string: " + reversed);
+
+    }
 
 }
