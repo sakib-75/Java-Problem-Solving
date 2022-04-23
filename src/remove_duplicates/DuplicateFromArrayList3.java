641f28a6ea8a8f81c8cfffe7ab64cc1bf3af636d
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-04-23 17:53:46 +0600

    DuplicateFromArrayList3 update

--- src/remove_duplicates/DuplicateFromArrayList3.java
+++ src/remove_duplicates/DuplicateFromArrayList3.java
@@ -7,32 +7,18 @@ import java.util.Set;
 
 public class DuplicateFromArrayList3 {
 
-    public static ArrayList<Integer> removeDuplicates(ArrayList<Integer> list)
-    {
-        Set<Integer> set = new LinkedHashSet<>();
-        set.addAll(list);
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 10, 1, 2, 2, 3, 10, 3, 3, 4, 5, 5));
+
+        Set<Integer> set = new LinkedHashSet<>(list);
 
         list.clear();
         list.addAll(set);
 
-        return list;
-    }
-
-    // Driver code
-    public static void main(String[] args)
-    {
-
-        // Get the ArrayList with duplicate values
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 10, 1, 2, 2, 3, 10, 3, 3, 4, 5, 5));
-
-        // Print the Arraylist
         System.out.println("ArrayList with duplicates: " + list);
+        System.out.println("ArrayList with duplicates removed: " + list);
 
-        // Remove duplicates
-        ArrayList<Integer> newList = removeDuplicates(list);
-
-        // Print the ArrayList with duplicates removed
-        System.out.println("ArrayList with duplicates removed: " + newList);
     }
 
 }
