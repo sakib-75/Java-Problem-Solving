55bad309d7cee92836158e24a40f27367af85e68
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-04-28 14:51:38 +0600

    JavaHashMap update

--- src/java_collection/JavaHashMap.java
+++ src/java_collection/JavaHashMap.java
@@ -1,6 +1,7 @@
 package java_collection;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class JavaHashMap {
 
@@ -19,6 +20,26 @@ public class JavaHashMap {
         if (map.containsKey("s")) {
             Integer a = map.get("s");
             System.out.println("value for key is: " + a);
+        }
+
+        // Using keySet() to get the set view of keys
+        System.out.println("The key set is: " + map.keySet());
+
+        // Using values() to get the set view of values
+        System.out.println("The collection is: " + map.values());
+
+        // Getting the value
+        System.out.println("The Value is: " + map.get("c"));
+
+        // Checking for the key_element
+        System.out.println("Is the key 's' present? " + map.containsKey("s"));
+
+        // Checking for the value
+        System.out.println("Is the value '340' present? " + map.containsValue(340));
+
+        // Iterate the map using for-each loop
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            System.out.println("Key: " + e.getKey() + ", Value: " + e.getValue());
         }
 
     }
